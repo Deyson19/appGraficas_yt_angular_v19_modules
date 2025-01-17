ebbda6aae6e49ac8e42726ca5a6cfcc4e169f89d
Deyson Venté <deiandresvent@hotmail.com>
2025-01-17 10:42:32 -0500

    Add routing configuration for home and chart components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarComponent } from '@Components/graficas/bar/bar.component';
+import { CircularComponent } from '@Components/graficas/circular/circular.component';
+import { LineComponent } from '@Components/graficas/line/line.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: async () => {
+      const m = await import('@Pages/layout/layout.component');
+      return m.default;
+    },
+  },
+  {
+    path: 'circular-chart',
+    component: CircularComponent,
+  },
+  {
+    path: 'line-chart',
+    component: LineComponent,
+  },
+  {
+    path: 'bar-chart',
+    component: BarComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
