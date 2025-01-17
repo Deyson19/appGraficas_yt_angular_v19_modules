030ba68d2194fddaf359a3ae7717c128f3593c33
Deyson Venté <deiandresvent@hotmail.com>
2025-01-17 10:41:09 -0500

    Add index file for components
    Create an index file to export all components from the components directory

---
+++ src/app/components/index.ts
@@ -0,0 +1,4 @@
+export { BarComponent } from './graficas/bar/bar.component';
+export { CircularComponent } from './graficas/circular/circular.component';
+export { LineComponent } from './graficas/line/line.component';
+export { NavbarComponent } from './navbar/navbar.component';
