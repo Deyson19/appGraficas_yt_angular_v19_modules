7071ee5f0054972d70602bc384fa17d3c5f25440
Deyson Venté <deiandresvent@hotmail.com>
2025-01-17 10:46:07 -0500

    Add necessary imports and providers to AppModule
    Updated AppModule to include additional imports and providers for RouterLink, HttpClientModule, BrowserAnimationsModule, ToastrModule, and ng2-charts. Also, configured ToastrModule with specific options and added experimental zoneless change detection.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
-import { NgModule } from '@angular/core';
+import {
+  NgModule,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterLink } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+import {
+  CircularComponent,
+  NavbarComponent,
+  LineComponent,
+  BarComponent,
+} from '@Components/';
+//*ng2-charts - chart.js Angular
+import {
+  BaseChartDirective,
+  provideCharts,
+  withDefaultRegisterables,
+} from 'ng2-charts';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CircularComponent,
+    NavbarComponent,
+    LineComponent,
+    BarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BaseChartDirective,
+    RouterLink,
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-right',
+      timeOut: 3000,
+      progressBar: true,
+      progressAnimation: 'increasing',
+      preventDuplicates: true,
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideHttpClient(),
+    provideAnimations(),
+    provideCharts(withDefaultRegisterables()),
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
